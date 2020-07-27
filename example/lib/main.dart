6199bd72d63158ed3e123c38d6f8108458b1ad8a
Kamil Klyta <kamil.klyta@htdevelopers.com>
2020-07-27 10:16:14 +0200

    Update example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -30,8 +30,40 @@ class MyHomePage extends StatelessWidget {
             element: StyleElement.geometry.fill,
             feature: StyleFeature.landscape.natural,
             rules: <StyleRule>[
-              StyleRule.hue(Colors.blue),
-              StyleRule.lightness(-10),
+              StyleRule.color(Colors.grey),
+            ],
+          ),
+          MapStyle(
+            feature: StyleFeature.water,
+            rules: <StyleRule>[
+              StyleRule.color(Colors.grey),
+              StyleRule.lightness(-30),
+            ],
+          )
+        ],
+        paths: <Path>[
+          Path(
+            weight: 2,
+            color: Colors.blue,
+            fillColor: Colors.purple.withOpacity(0.5),
+            points: <Location>[
+              Location(-3.220028, -59.924832),
+              Location(-3.234028, -59.964832),
+              Location(-3.265628, -59.994832),
+              Location(-3.261128, -59.947832),
+              Location(-3.216728, -59.895832),
+              Location(-3.220028, -59.924832),
+            ],
+          ),
+          Path(
+            weight: 3,
+            color: Colors.blue,
+            points: <Location>[
+              Location(-3.352538, -60.163816),
+              Location(-3.314760, -60.050712),
+              Location(-3.364583, -59.942379),
+              Location(-3.268667, -59.901430),
+              Location(-3.198791, -59.876131),
             ],
           )
         ],
